6a366913e6c64aa47d3aa39a57fba2ffeec02fcf
Jason Sears <jsears@rightpoint.com>
2018-07-28 13:38:54 -0500

    Added appsettings.json

--- Ocr/Ocr.App/Program.cs
+++ Ocr/Ocr.App/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace Ocr.App
 {
@@ -6,6 +8,14 @@ namespace Ocr.App
     {
         public static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            Console.WriteLine(configuration["subscriptionKey"]);
+            Console.ReadLine();
         }
     }
 }
